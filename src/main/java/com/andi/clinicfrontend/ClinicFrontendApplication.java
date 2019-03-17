2325b44cfc162bf7341f89805f1459f643cd8b65
Andi <familie.mayer@aon.at>
2019-03-17 11:03:19 +0100

    first commit

---
+++ src/main/java/com/andi/clinicfrontend/ClinicFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.andi.clinicfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicFrontendApplication.class, args);
+    }
+
+}
